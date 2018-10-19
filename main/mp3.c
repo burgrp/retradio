5b0de350b5ec978bbba3523188502edbd8a0971f
Pavel Burgr <pb@drake.cz>
2018-10-19 02:35:34 +0200

    WIP

--- main/mp3.c
+++ main/mp3.c
@@ -11,6 +11,7 @@ const int CHECK_BUFFER_BIT = BIT0;
 const int BUFFER_ACCEPTING_BIT = BIT1;
 
 static mp3dec_t mp3d;
+short pcm[MINIMP3_MAX_SAMPLES_PER_FRAME];
 
 void log_buffer(char *condition)
 {
@@ -21,12 +22,12 @@ void mp3_push_data(unsigned char *data, int len)
 {
     xEventGroupWaitBits(mp3_event_group, BUFFER_ACCEPTING_BIT, false, true, portMAX_DELAY);
 
-    log_buffer("push enter");
+    log_buffer("before push");
     for (int c = 0; c < len && size < sizeof(buffer); c++)
     {
         buffer[size++] = data[c];
     }
-    log_buffer("push exit");
+    log_buffer("after push");
 
     if (size > 16000)
     {
@@ -41,26 +42,34 @@ void mp3_decoder_task(void *param)
         xEventGroupWaitBits(mp3_event_group, CHECK_BUFFER_BIT, false, true, portMAX_DELAY);
         xEventGroupClearBits(mp3_event_group, CHECK_BUFFER_BIT);
 
-        log_buffer("MP3 input - pull enter");
-
         mp3dec_frame_info_t info;
-        short pcm[MINIMP3_MAX_SAMPLES_PER_FRAME];
-        int samples = mp3dec_decode_frame(&mp3d, buffer, size, pcm, &info);
-        LOG_INFO("MP3 decoder consumed %d bytes, produced %d samples", info.frame_bytes, samples);
+        memset(&info, 0, sizeof(info));
+        do
+        {
+            log_buffer("before decode");
 
-        xEventGroupClearBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
-        for (int c = info.frame_bytes; c < size; c++) {
-            buffer[c - info.frame_bytes] = buffer[c];
-        }
-        xEventGroupSetBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
-        log_buffer("MP3 input - pull exit");
+            int samples = mp3dec_decode_frame(&mp3d, buffer, size, pcm, &info);
+            LOG_INFO("MP3 decoder consumed %d bytes, produced %d samples", info.frame_bytes, samples);
+
+            xEventGroupClearBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
+            for (int c = 0; c < info.frame_bytes; c++)
+            {
+                buffer[c] = buffer[c + info.frame_bytes];
+            }
+            size -= info.frame_bytes;
+            xEventGroupSetBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
+
+            log_buffer("after decode");
+        } while (info.frame_bytes);
+        
     }
 }
 
 void mp3_init()
 {
     mp3dec_init(&mp3d);
-    xEventGroupSetBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
+    memset(&mp3d, 0, sizeof(mp3d));
     mp3_event_group = xEventGroupCreate();
-    xTaskCreate(mp3_decoder_task, "mp3_decoder_task", DEFAULT_STACK_SIZE, NULL, 0, NULL);
+    xEventGroupSetBits(mp3_event_group, BUFFER_ACCEPTING_BIT);
+    xTaskCreate(mp3_decoder_task, "mp3_decoder_task", 30000, NULL, 0, NULL);
 }
