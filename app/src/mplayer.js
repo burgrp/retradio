8e555948589e0629fc164a093f4982c259769537
Pavel Burgr <pb@drake.cz>
2019-02-20 20:27:31 +0100

    WIP

--- app/src/mplayer.js
+++ app/src/mplayer.js
@@ -10,18 +10,33 @@ module.exports = async config => {
 
     let stations = config.stations;
     let bandIndex = 0;
-    let stationIndex = 0;
+    let stationIndex = 1;
+    let trebleLevel = 0;
+    let bassLevel = 0;
+    let aperture;
+
+    function getEqualizer() {
+        //let levels = [bassLevel, bassLevel, 2 * bassLevel / 3, bassLevel / 3, 0, 0, trebleLevel / 3, 2 * trebleLevel / 3, trebleLevel, trebleLevel];
+        let levels = [bassLevel, 2 * bassLevel / 3, bassLevel / 3, 0, 0, 0, 0, trebleLevel / 3, 2 * trebleLevel / 3, trebleLevel];
+        //let levels = [bassLevel, bassLevel / 2, 0, 0, 0, 0, 0, 0, trebleLevel / 2, trebleLevel];
+        return levels.map(l => l / 2 - 6).join(":");
+    }
 
     function mpStart() {
 
-        mplayer = spawn("mplayer", [
+        let params = [
             "-ao",
             "alsa",
             "-quiet",
             "-nolirc",
             "-slave",
+            "-af",
+            "equalizer=" + getEqualizer() + ",stats,volnorm",
             stations[bandIndex][stationIndex]
-        ]);
+        ];
+
+        info("mplayer", ...params);
+        mplayer = spawn("mplayer", params);
 
         mplayer.stdout.on("data", (data) => {
             mplayerOut(data.toString());
@@ -53,22 +68,23 @@ module.exports = async config => {
         await asyncWait(2000);
     }
 
+    async function changeEqualizer() {
+        await mpDo("af_cmdline", "equalizer", getEqualizer());
+    }
+
+    function changeValue(current, change, min, max) {
+        return Math.max(Math.min(current + change, max), min);
+    }
+
     mpStart();
 
     return {
         async changeVolume(change) {
-            info(`Change volume: ${change}`);
             await mpDo("volume", change);
         },
 
         async changeTuning(change) {
-            stationIndex += change;
-            if (stationIndex < 0) {
-                stationIndex = 0;
-            }
-            if (stationIndex >= stations[bandIndex].length) {
-                stationIndex = stations[bandIndex].length - 1;
-            }
+            stationIndex = changeValue(stationIndex, change, 0, stations[bandIndex].length - 1);
             await changeStation();
         },
 
@@ -79,20 +95,41 @@ module.exports = async config => {
 
         async changeBand(change) {
             stationIndex = 0;
-            bandIndex += change;
-            if (bandIndex < 0) {
-                bandIndex = 0;
-            }
-            if (bandIndex >= stations.length) {
-                bandIndex = stations.length - 1;
-            }
+            bandIndex = changeValue(bandIndex, change, 0, stations.length - 1);
             await changeStation();
+        },
+
+        async changeTreble(change) {
+            trebleLevel = changeValue(trebleLevel, change, -12, 12);
+            await changeEqualizer();
+        },
+
+        async resetTreble() {
+            trebleLevel = 0;
+            await changeEqualizer();
+        },
+
+        async changeBass(change) {
+            bassLevel = changeValue(bassLevel, change, -12, 12);
+            await changeEqualizer();
+        },
+
+        async resetBass() {
+            bassLevel = 0;
+            await changeEqualizer();
         },
 
         async resetBand() {
             stationIndex = 0;
             bandIndex = 0;
-            changeStation();
+            await changeStation();
+        },
+
+        async toggleAperture() {
+            aperture = !aperture;
+            trebleLevel = aperture ? -3 : +10;
+            bassLevel = aperture ? -12 : +12;
+            await changeEqualizer();
         }
     }
 }
