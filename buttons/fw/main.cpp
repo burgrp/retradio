7d932b0b941c16d6b4ff5835677f673efc05d59c
Pavel Burgr <pb@drake.cz>
2021-11-27 19:43:05 +0100

    WIP

--- buttons/fw/main.cpp
+++ buttons/fw/main.cpp
@@ -1,14 +1,9 @@
 const int PIN_ADDR1 = 8;
 const int PIN_ADDR2 = 9;
 
-const int PIN_SAFEBOOT = 25;
+const int PIN_SAFEBOOT = 16;
 
-const int PIN_ENC_A = 2;
-const int PIN_ENC_B = 4;
-const int EXT_INT_ENC_A = 2;
-const int EXT_INT_ENC_B = 4;
-
-const int PIN_ENC_PUSH = 24;
+const int PIN_BUTTONS[8] = {23, 24, 25, 2, 3, 4, 6, 5};
 
 const int PIN_SDA = 14;
 const int PIN_SCL = 15;
@@ -23,8 +18,7 @@ public:
 
     struct {
       unsigned char protocol = 2;
-      int position = 0;
-      unsigned char flags = 0;
+      unsigned char buttons = 0;
     } __attribute__((packed)) txData;
 
     void init(Device *that, int address) {
@@ -34,24 +28,9 @@ public:
                   target::port::PMUX::PMUXE::C);
     }
 
-    virtual int getTxByte(int index) {
-      if (index == 0) {
-        txData.position = that->encoder.position;
-      }
-      return index < sizeof(txData) ? ((unsigned char *)&txData)[index] : 0;
-    }
+    virtual int getTxByte(int index) { return index < sizeof(txData) ? ((unsigned char *)&txData)[index] : 0; }
 
   } slave;
-
-  class : public atsamd::encoder::Encoder {
-  public:
-    Device *that;
-
-    void init(Device *that) {
-      this->that = that;
-      Encoder::init(PIN_ENC_A, PIN_ENC_B, EXT_INT_ENC_A, EXT_INT_ENC_B);
-    }
-  } encoder;
 
   class : public genericTimer::Timer {
   public:
@@ -62,14 +41,24 @@ public:
       start(KEY_TIMER_MS / 10);
     }
 
-    void onTimer() { 
-      that->slave.txData.flags = !(target::PORT.IN >> PIN_ENC_PUSH & 1);
-      start(KEY_TIMER_MS / 10); 
+    void onTimer() {
+
+      int states = 0;
+      for (int button = 0; button < 8; button++) {
+        int pin = PIN_BUTTONS[button];
+        states |= (!(target::PORT.IN >> pin & 1) & 1) << button;
+      }
+
+      that->slave.txData.buttons = states;
+      
+      start(KEY_TIMER_MS / 10);
     }
 
   } timer;
 
   void init() {
+
+    // TC1 for motor PWM
 
     target::PM.APBCMASK.setTC(1, true);
 
@@ -81,10 +70,9 @@ public:
     while (target::GCLK.STATUS.getSYNCBUSY())
       ;
 
-    encoder.init(this);
     timer.init(this);
 
-    int address = 0x40 + atsamd::configPin::readConfigPin(PIN_ADDR1) + 3 * atsamd::configPin::readConfigPin(PIN_ADDR2);
+    int address = 0x49 + atsamd::configPin::readConfigPin(PIN_ADDR1) + 3 * atsamd::configPin::readConfigPin(PIN_ADDR2);
     slave.init(this, address);
   }
 };
@@ -92,7 +80,6 @@ public:
 Device device;
 
 void interruptHandlerSERCOM0() { device.slave.interruptHandlerSERCOM(); }
-void interruptHandlerEIC() { device.encoder.interruptHandlerEIC(); }
 
 void initApplication() {
 
@@ -108,11 +95,12 @@ void initApplication() {
   // enable interrupts
   target::NVIC.IPR[target::interrupts::External::SERCOM0 >> 2].setPRI(target::interrupts::External::SERCOM0 & 0x03, 3);
   target::NVIC.ISER.setSETENA(1 << target::interrupts::External::SERCOM0);
-  target::NVIC.ISER.setSETENA(1 << target::interrupts::External::EIC);
 
-  // configure push button
-  target::PORT.DIRCLR = 1 << PIN_ENC_PUSH;
-  target::PORT.PINCFG[PIN_ENC_PUSH].setINEN(true).setPULLEN(true);
-  target::PORT.OUTSET = 1 << PIN_ENC_PUSH;
-
+  // configure buttons
+  for (int button = 0; button < 8; button++) {
+    int pin = PIN_BUTTONS[button];
+    target::PORT.DIRCLR = 1 << pin;
+    target::PORT.PINCFG[pin].setINEN(true).setPULLEN(true);
+    target::PORT.OUTSET = 1 << pin;
+  }
 }
